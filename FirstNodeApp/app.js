f1887f5a3ab8d31da39c81215ce897cf80d3217a
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2022-06-23 14:19:08 +0530

    write a file

--- FirstNodeApp/app.js
+++ FirstNodeApp/app.js
@@ -73,4 +73,6 @@ abc.emit("speak", "Hello All!!");
 var fs = require("fs");
 
 var readMe = fs.readFileSync("readMe.txt", "utf8");
-console.log(readMe); 
+
+fs.writeFileSync("writeMe.txt", readMe);
+
