e334fe683dd5b5081fc3486d4eab7d425eb46c86
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2022-06-24 18:50:17 +0530

    making dir and creating files

--- FirstNodeApp/app.js
+++ FirstNodeApp/app.js
@@ -80,6 +80,12 @@ fs.writeFileSync("writeMe.txt", readMe);
 
 var fs = require("fs");
 
-fs.mkdirSync("stuff");
+fs.mkdir("stuff", function()
+{
+    fs.readFile("readMe.txt", "utf8", function(err, data)
+    {
+        fs.writeFile("./stuff/writeMe.txt", data)
+    });
+});
 
 
