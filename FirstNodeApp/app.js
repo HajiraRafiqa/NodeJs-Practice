64909cb968191d10d3ddea5e34aaf063f79339af
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2022-06-25 13:20:49 +0530

    writing a stream

--- FirstNodeApp/app.js
+++ FirstNodeApp/app.js
@@ -4,9 +4,11 @@ var fs = require("fs");
 const path = require('path');
 
 var myReadStream = fs.createReadStream(path.join(__dirname, "/readMe.txt"), 'utf8');
+var myWriteStream = fs.createWriteStream(path.join(__dirname, "/writeMe.txt"));
 
 myReadStream.on("data", function(chunk)
 {
     console.log("new chunk recieved:");
-    console.log(chunk); 
+    myWriteStream.write(chunk);
+
 });
