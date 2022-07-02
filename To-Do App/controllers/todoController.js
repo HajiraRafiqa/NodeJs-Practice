94f130235c37f770061e70d0434bed68be994e07
HajiraRafiqa <hajira.rafiqa@capgemini.com>
2022-07-02 17:42:41 +0530

    making the controller

---
+++ To-Do App/controllers/todoController.js
@@ -0,0 +1,19 @@
+module.exports = function(app)
+{
+    app.get("/todo", function(req, res)
+    {
+
+    });
+
+    
+    app.get("/todo", function(req, res)
+    {
+        
+    });
+
+    app.delete("/todo", function(req, res)
+    {
+        
+    });
+
+};
--- To-Do App/public/app.js
+++ To-Do App/public/app.js
@@ -2,11 +2,17 @@ var express = require("express");
 
 var app = express;
 
+var todoController = require("./Controllers/todoController")
+
 //set up template engine 
 app.set("view engine", "ejs");
 
 //static files 
 app.use(express.static("public"))
+
+//file controllers
+todoController(app);
+
 
 //listen to port 
 app.listen(3000);
